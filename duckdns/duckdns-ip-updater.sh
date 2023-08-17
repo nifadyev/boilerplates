c9c5213c6b9d0f407a2486d45d05844beaddafa7
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2023-08-17 16:21:10 +0300

    Add systemd periodic task for updating ip address for DuckDNS

---
+++ duckdns/duckdns-ip-updater.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+RESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=")
+
+if [ "${RESPONSE}" = "OK" ]; then
+    echo "Your IP was updated at $(date)"
+else
+    echo -e "Something went wrong, please check your settings $(date)\nThe response returned was:\n${RESPONSE}"
+fi
