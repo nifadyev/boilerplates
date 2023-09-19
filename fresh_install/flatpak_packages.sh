460f1244c93ac1a967603a1f4cb84b855ba5a2fd
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2023-09-19 11:27:38 +0300

    Add script to install necessary Flatpak packages

---
+++ fresh_install/flatpak_packages.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+    # Break reminder
+    # (config is stored at `~/.config/safeeyes/safeeyes.json`)
+    # flathub io.github.slgobinath.SafeEyes \
+    # flathub net.ankiweb.Anki
+
+flatpak install \
+    io.github.slgobinath.SafeEyes \
+    com.github.iwalton3.jellyfin-media-player \
+    flathub net.ankiweb.Anki
