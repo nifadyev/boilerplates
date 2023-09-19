392ac8bb0720a08ac92a733502fb13edcf350107
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2023-09-19 11:34:16 +0300

    Add script to setup vscodium

---
+++ fresh_install/vscoduim.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Source - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo
+
+sudo tee -a /etc/yum.repos.d/vscodium.repo << 'EOF'
+[gitlab.com_paulcarroty_vscodium_repo]
+name=gitlab.com_paulcarroty_vscodium_repo
+baseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg
+metadata_expire=1h
+EOF
+
+sudo dnf install codium
