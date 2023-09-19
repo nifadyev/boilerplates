fdb566546083cc49bc6aa411e3918d6ab5a04d8b
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2023-09-19 11:26:36 +0300

    Add script to install necessary rpm packages

---
+++ fresh_install/dnf_packages.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+
+# Speed up and optimization
+echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
+echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
+echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf
+
+# vlc
+# tilix
+# httpie
+# telegram-desktop
+# fira-code-fonts
+# gnome-tweaks
+# # Required for Unite GNOME extension
+# xprop
+# # Torrent client
+# transmission
+# timeshift
+# gnome-extensions-app
+# dconf-editor
+# lollypop # Music player
+# yt-dlp
+# bleachbit - System cleaner
+# Steam
+# Restic - old version in rpm, Backup tool
+
+sudo dnf install \
+    vlc \
+    tilix \
+    httpie \
+    telegram-desktop \
+    fira-code-fonts \
+    gnome-tweaks \
+    xprop \
+    transmission \
+    timeshift \
+    gnome-extensions-app \
+    dconf-editor \
+    lollypop \
+    yt-dlp \
+    bleachbit \
+    steam \
+    restic
