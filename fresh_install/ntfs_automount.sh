97fcbe370e718b3cfef7593c5a4a2508d82d8168
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2023-09-19 11:31:33 +0300

    Add template to auto-mount data partition

---
+++ fresh_install/ntfs_automount.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Find partition and copy its UUID (preferably, by label/SYSTEM)
+sudo lsblk -f | grep ntfs
+# Create mount folder with user permissions
+mkdir ~/Storage
+# TODO: wait for input here, uncomment commands below when it's done
+# Append mount details
+# echo "UUID="<partition_uuid>" /home/<username>/Storage    ntfs-3g defaults    0 0" | sudo tee --append /etc/fstab
+# Mount all partitions and check if NTFS partition is mounted correctly
+# sudo mount -a
