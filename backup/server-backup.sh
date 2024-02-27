835307d333b7714d9b63a4917f213a25398fb9d2
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2024-02-27 17:46:55 +0300

    Add comment about quoting env variable

--- backup/server-backup.sh
+++ backup/server-backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# ! Do not add quotes around EXCLUDE_PATTERNS otherwise systemd task will fail
 restic --repo $REPOSITORY_PATH \
     backup $BACKUP_PATTERNS \
     $EXCLUDE_PATTERNS --exclude-caches
