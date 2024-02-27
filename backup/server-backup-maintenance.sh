4b4cddf4a451e216eb8b6004f1c3c013e1f93093
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2024-02-27 09:04:04 +0300

    Add pruning redundant data and checking repo validity to backup maintenance script

--- backup/server-backup-maintenance.sh
+++ backup/server-backup-maintenance.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-restic --repo $REPOSITORY_PATH forget \
+# Delete old snapshots
+# `--prune` option - remove the remaining data that was referenced only by the removed snapshots
+restic --repo "$REPOSITORY_PATH" forget \
+    --cleanup-cache \
     --keep-hourly 1 \
     --keep-daily 7 \
     --keep-weekly 4 \
     --keep-monthly 3 \
-    --keep-yearly 1
+    --keep-yearly 1 \
+    --prune
+
+# Validate repository health
+restic --repo "$REPOSITORY_PATH" check
