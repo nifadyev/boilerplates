4e3c66b461f899b8fb9dbb5a0fdc33fa80f624b8
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2024-02-27 09:45:15 +0300

    Add command to remove old backups

--- vaultwarden/backup.sh
+++ vaultwarden/backup.sh
@@ -4,4 +4,6 @@ cd "$VAULTWARDEN_DATA_PATH" || return
 
 sqlite3 data/db.sqlite3 "VACUUM INTO '${BACKUP_PATH}/db-$(date '+%Y%m%d-%H%M').sqlite3'"
 
+find "$VAULTWARDEN_BACKUP_PATH" -wholename "$VAULTWARDEN_BACKUP_PATH/*.sqlite3" -daystart -mtime +"$VAULTWARDEN_MAX_BACKUPS_COUNT" -delete
+
 cd - || return
