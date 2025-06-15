e748c6e8d3e50a139e67907698509a399a5093f5
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2025-06-15 19:54:59 +0300

    Remove extra packages from dnf script

--- fresh_install/dnf_packages.sh
+++ fresh_install/dnf_packages.sh
@@ -6,42 +6,24 @@ echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
 echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf
 
 # vlc
-# tilix
-# httpie
-# telegram-desktop
+# curlie
 # fira-code-fonts
-# gnome-tweaks
-# # Required for Unite GNOME extension
-# xprop
 # # Torrent client
 # transmission
 # timeshift
-# gnome-extensions-app
 # dconf-editor
-# lollypop # Music player
-# yt-dlp
 # bleachbit - System cleaner
-# Steam
 # Restic - old version in rpm, Backup tool
 # Kitty - Terminal, might not be the actual version, use installation script from repo for the latest version
 # zsh - alternative shell
 
 sudo dnf install \
-    vlc \
-    tilix \
-    httpie \
-    telegram-desktop \
+    curlie \
     fira-code-fonts \
-    gnome-tweaks \
-    xprop \
     transmission \
     timeshift \
-    gnome-extensions-app \
     dconf-editor \
-    lollypop \
-    yt-dlp \
     bleachbit \
-    steam \
     restic \
     kitty \
     zsh
