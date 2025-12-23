52bff26dcfcba20e76d5042d6ba87c22a076f11d
Vadim Nifadev <36514612+nifadyev@users.noreply.github.com>
2025-12-23 19:05:18 +0400

    Delete cofnigs of no more used services

--- duckdns/duckdns-ip-updater.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-RESPONSE=$(curl -sS --max-time 60 "https://www.duckdns.org/update?domains=${SUBDOMAINS}&token=${TOKEN}&ip=")
-
-if [ "${RESPONSE}" = "OK" ]; then
-    echo "Your IP was updated at $(date)"
-else
-    echo -e "Something went wrong, please check your settings $(date)\nThe response returned was:\n${RESPONSE}"
-fi
